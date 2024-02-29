b338e06abeb13c171ed94b45f4d5e50cd3754156
sugar404 <sugar404@qq.com>
2024-02-29 19:37:08 +0800

    字体样式修改

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -101,7 +101,7 @@ watch(
       width: 100%;
       padding-left: 22px;
       transform: translateY(-8px);
-      font-family: "Pacifico-Regular";
+      font-family: "Sriacha-Regular";
 
       .bg {
         font-size: 5rem;
